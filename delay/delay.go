ae0e90dff02e493f182b4d0e06a6d1de8c621394
Yago Nobre <yago.santos@luizalabs.com>
2017-09-20 23:09:17 -0300

    add delay histogram

--- delay/delay.go
+++ delay/delay.go
@@ -3,19 +3,45 @@ package delay
 import (
 	"math/rand"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
+var (
+	delayDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "delay_durations_histogram_seconds",
+		Help: "The latency distributions by func",
+	}, []string{"func"})
+)
+
+func init() {
+	prometheus.MustRegister(delayDuration)
+}
+
 func Delay1() {
-	r := rand.Intn(10)
+	now := time.Now()
+
+	r := rand.Intn(100)
 	time.Sleep(time.Duration(r) * time.Millisecond)
+
+	delayDuration.WithLabelValues("deploy1").Observe(time.Since(now).Seconds())
 }
 
 func Delay2() {
-	r := rand.Intn(100)
+	now := time.Now()
+
+	r := rand.Intn(1000)
 	time.Sleep(time.Duration(r) * time.Millisecond)
+
+	delayDuration.WithLabelValues("deploy2").Observe(time.Since(now).Seconds())
+
 }
 
 func Delay3() {
-	r := rand.Intn(500)
+	now := time.Now()
+
+	r := rand.Intn(1500)
 	time.Sleep(time.Duration(r) * time.Millisecond)
+
+	delayDuration.WithLabelValues("deploy3").Observe(time.Since(now).Seconds())
 }
