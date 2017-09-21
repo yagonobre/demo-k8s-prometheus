9f7771fb0b617ce15feea79168389c7b296160d1
Yago Nobre <yago.santos@luizalabs.com>
2017-09-21 00:17:43 -0300

    add handlers.go

--- main.go
+++ main.go
@@ -1,31 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-
-	"github.com/yagonobre/demo-k8s-prometheus/delay"
 )
-
-func hostname(w http.ResponseWriter, r *http.Request) {
-	hn, _ := os.Hostname()
-	fmt.Fprintf(w, hn)
-}
-
-func hostnameWithDelay(w http.ResponseWriter, r *http.Request) {
-	delay.Delay1()
-	delay.Delay2()
-	delay.Delay3()
-
-	hn, _ := os.Hostname()
-	fmt.Fprintf(w, hn)
-}
 
 func server() {
 	r := mux.NewRouter().StrictSlash(true)
